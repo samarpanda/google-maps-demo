e103c89221fe917879f9f7ab9a08b913c4bc4f9f
Samar Panda <er.samarpanda@gmail.com>
2024-12-23 00:33:59 +0530

    fix: playwright github action ci test

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Maximum time each action i.e `click()` can take */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -83,7 +83,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:3000",
+    url: "http://localhost:5173",
     reuseExistingServer: true,
   },
 });
